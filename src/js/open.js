73eb2456c87efe6ec531c24e1fbe8db5d163cbe4
pipu11qiao <18854558196@163.com>
2019-07-11 19:48:08 +0800

    update

--- src/js/open.js
+++ src/js/open.js
@@ -9,8 +9,16 @@ var tempUrl = 'http://zx-math.test.17zuoye.net/v/index';
 var realUrl = tempUrl;
 var c = 1;
 $(document.body).on('click', '.btn', function() {
-    var tempWin = window.open(tempUrl, 'name' + (c++), '_black ');
-    $.get(requetUrl, function() {
-        tempWin.location.href = realUrl
-    })
+    // var tempWin = window.open(tempUrl, 'name' + (c++), '_black ');
+    var tempWin = window.open(tempUrl, '_blank');
+    setTimeout(function() {
+
+        tempWin.close();
+    },500)
+    // $.get(requetUrl, function() {
+    // if(tempWin){
+    // }else{
+    // tempWin.location.href = realUrl
+    // }
+    // })
 });
